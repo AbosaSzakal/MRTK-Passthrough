08b5546cf2fa97baf1b6dc04179024c913fdf8d7
AbosaSzakal <abosa99.developer@gmail.com>
2021-08-15 21:00:00 +0200

    First build

---
+++ Assets/MRTK-Passthrough/ClearHands.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearHands : MonoBehaviour
+{
+    public GameObject parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitAndClear());
+    }
+
+    IEnumerator WaitAndClear(){
+        yield return new WaitForSeconds(1);
+        ClearAllHands();
+    }
+
+    public void ClearAllHands(){
+        foreach(Transform tr in parent.transform){
+            if(tr.name.Contains("RiggedHand")){
+                Destroy(tr.gameObject);
+            }
+        }
+    }
+
+
+}
---
+++ Assets/MRTK-Passthrough/ConsoleToGUI.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ConsoleToGUI : MonoBehaviour
+ {
+     public string myLog = "*begin log";
+     string filename = "";
+     public bool doShow = true;
+     public Text textBox;
+     int kChars = 8000;
+     //void OnEnable() { Application.logMessageReceived += Log; text = GetComponent<Text>();}
+     //void OnDisable() { Application.logMessageReceived -= Log; text = GetComponent<Text>();}
+
+     private void Awake() {
+         Application.logMessageReceived += Log;
+     }
+     void Update() { 
+         
+        //if (Input.GetKeyDown(KeyCode.Space)) { doShow = !doShow; } 
+        if(textBox != null)
+            textBox.text = myLog;
+
+    }
+     public void Log(string logString, string stackTrace, LogType type)
+     {
+        // for onscreen...
+         myLog = myLog + "\n" + logString;
+         if (myLog.Length > kChars) { myLog = myLog.Substring(myLog.Length - kChars); }
+ 
+         // for the file ...
+         if (!(filename == ""))
+         {
+             string d = System.Environment.GetFolderPath(
+                System.Environment.SpecialFolder.Desktop) + "/YOUR_LOGS";
+             System.IO.Directory.CreateDirectory(d);
+             string r = Random.Range(1000, 9999).ToString();
+             filename = d + "/log-" + r + ".txt";
+         }
+         try { System.IO.File.AppendAllText(filename, logString + "\n"); }
+         catch { }
+     }
+ 
+     void OnGUI()
+     {
+         if (!doShow) { return; }
+         GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity,
+            new Vector3(Screen.width / 1200.0f, Screen.height / 800.0f, 1.0f));
+         GUI.TextArea(new Rect(10, 10, 540, 370), myLog);
+     }
+ }
